77415725427c734f6928dc4905ee64ce9c941767
Kyrsten <ksurbayan@gmail.com>
2021-11-04 12:09:31 -0800

    setup model associations

--- models/index.js
+++ models/index.js
@@ -5,12 +5,28 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'id'
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'id'
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  as: 'product_tag',
+  foreignKey: 'id'
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  as: 'product_name',
+  foreignKey: 'id'
+});
 
 module.exports = {
   Product,
